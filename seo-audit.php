e11397a8546def7f52c7b83732e17a063d76d5fc
jbeltrami <joao_beltrami@hotmail.com>
2017-08-25 11:44:11 -0400

    Add content to SEO Audit

--- seo-audit.php
+++ seo-audit.php
@@ -23,8 +23,40 @@
     <div class="">
     <h2>.htaccess file</h2>
       <p>Declare cannonical URL, gzip and other configurations for the Server Software to read before serving the content. Always located in the root of the project, but more specific .htaccess files can also be included in sub-menus</p>
-      
+
+      <p>Examples:</p>
+      <ul>
+        <li>Error documents</li>
+        <li>Redirects</li>
+        <li>Password protection</li>
+        <li>Deny visitors by IP address</li>
+        <li>Deny visitors by referrer</li>
+        <li>Hot link prevention techniques</li>
+        <li>Blocking offline browsers and 'bad bots'</li>
+        <li>DirectoryIndex uses</li>
+        <li>Adding MIME types</li>
+        <li>Enable SSI with .htaccess</li>
+        <li>Enable CGI outside of the cgi-bin</li>
+        <li>Disable directory listings</li>
+        <li>Setting server timezone</li>
+        <li>Changing server signature</li>
+        <li>Preventing access to your PHP includes files</li>
+        <li>Prevent access to php.ini</li>
+        <li>Forcing scripts to display as source code</li>
+        <li>Ensuring media files are downloaded instead of played</li>
+        <li>Setting up Associations for Encoded Files</li>
+        <li>Preventing requests with invalid characters</li>
+      </ul>
+
       <p>Reference at: <a href="http://www.htaccess-guide.com/">.htaccess guide</a></p>
+    </div>
+
+    <div class="">
+      <h2>Header</h2>
+      <p>Metatags (further reading required)</p>
+
+      <p>Reference at: <a href="http://www.metatags.org/all_metatags">All metatags</a></p>
+      <p>Reference at: <a href="https://moz.com/blog/seo-meta-tags">Blog post about fundamental meta tags</p>
     </div>
 
   <?php include('footer.php'); ?>
